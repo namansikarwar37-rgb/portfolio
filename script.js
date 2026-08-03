28701ceb58267073c11b68b59088377976bf81dd
namansikarwar37-rgb <namansikarwar37@gmail.com>
2026-08-03 22:08:42 +0530

    Completed Day 8 - Added Projects section

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+// Welcome message
 console.log("Portfolio Loaded Successfully");
+
+// Know More button click
+const knowMoreBtn = document.querySelector(".btn");
+
+knowMoreBtn.addEventListener("click", function () {
+    alert("Welcome to my portfolio!");
+});
+
+// Scroll to Top Button
+const topBtn = document.getElementById("topBtn");
+
+// Show button after scrolling
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 300) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+});
+
+// Scroll back to top
+topBtn.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+const heading = document.querySelector(".hero h1");
+
+heading.style.color = "#1e293b";
+document.getElementById("year").textContent = new Date().getFullYear();
